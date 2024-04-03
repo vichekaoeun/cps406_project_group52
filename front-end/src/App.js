450f501e713ff15b183851a2022ae3ddc3c136aa
Vicheka Oeun <oeunvicheka95@gmail.com>
2024-04-02 21:53:45 -0400

    style login page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import ladybug from './images/ladybug.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Main-Container">
+      <div className="Content">
+        <div id="title-card">
+          <img src={ladybug} className="App-logo" alt="logo" />
+          <h1>Bug Report System (BRS)</h1>
+        </div>
+        <p><i>Login</i></p>
+        <div className="red-bar"></div>
+      </div>
+      <div className="Authentication">
+        <form id="form">
+          <label>
+            Username:
+            <input />
+          </label>
+          <label>
+            Password:
+            <input />
+          </label>
+          <button type='submit'>Login</button>
+        </form>
+        <div className="other-form">
+          <div id="forgot-form">
+            <button>*Forgot password</button>
+            <button>*Forgot username</button>
+          </div>
+          <div id="create-form">
+            <button>New User?</button>
+            <button>*Sign up now!</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
